f72a6fa7594950f69c297a4412de9171a8964def
sylvaincodes <adandesylvain2010@gmail.com>
2022-10-30 13:52:56 +0100

    page acceuil ajout section projets

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -5,8 +5,8 @@
 */ 
 
 const addEventOnElements = function (elements, eventType,callback) {
-    for (let index = 0; index < elements.length; index++) {
-        elements[index].addEventListener(eventType,callback);        
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener(eventType,callback);        
     }
 }
 
@@ -48,4 +48,79 @@ const closeNav = function () {
 addEventOnElements(navLinks, "click", closeNav);
 
 
+/**
+ * TEXT ANIMATION EFFECT HERO
+ */
+
+const letterBoxes = document.querySelectorAll("[data-letter-effect");
+let activeLetterBoxIndex = 0;
+let lastActiveLetterBoxIndex = 0;
+let totalLetterBoxDelay = 0;
+
+const setLetterEffect = function () {
+    
+    // Loop 
+    for (let i = 0; i < letterBoxes.length; i++) {
+        // set initial animation delay
+
+        let letterAnimationDelay = 0;
+
+        // get all characters
+        const letters = letterBoxes[i].textContent.trim();
+
+        //remove all chara 
+        letterBoxes[i].textContent =  "";
+
+        // loop
+        for (let j = 0; j < letters.length; j++) {
+            const span = document.createElement("span");
+
+            // set animation delay on span
+            span.style.animationDelay = letterAnimationDelay +'s';
+
+            if ( i === activeLetterBoxIndex) {
+                span.classList.add("in");
+            }else{
+                span.classList.add("out");
+            }
+
+
+            span.textContent = letters[j];
+
+            if(letters[j] === " "){
+                span.classList.add("space");
+            } 
+
+            letterBoxes[i].appendChild(span);
+            if (j>=letters.lenght-1) break;
+
+            letterAnimationDelay +=0.05;
+            
+        }
+
+        if (i === activeLetterBoxIndex) {
+            totalLetterBoxDelay =  Number(letterAnimationDelay.toFixed(2));
+        }
+
+        if( i === lastActiveLetterBoxIndex){
+            letterBoxes[i].classList.add("active");
+
+        }else{
+            letterBoxes[i].classList.remove("active");
+        }
+
+    }
+
+    setTimeout(() => {
+       lastActiveLetterBoxIndex = activeLetterBoxIndex;
+       
+       activeLetterBoxIndex >= letterBoxes.length - 1 ? activeLetterBoxIndex =0 : activeLetterBoxIndex++;
+
+       setLetterEffect();
+
+    }, (totalLetterBoxDelay * 1000) + 3000);
+}   
+
+
+window.addEventListener('load', setLetterEffect); 
 
